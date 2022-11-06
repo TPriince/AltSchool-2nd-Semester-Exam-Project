d52393f0c2cbf9853f7a07f8bd01aebe5e45092a
TPriince <Tobiloba110@gmail.com>
2022-11-06 10:41:54 +0100

    Update ErrorPage

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ErrorPage() {
   return (
     <div>
-      <h2>404 error: Page Not Found!</h2>
+      <h2 className="errorpage-header">404 error: Page Not Found!</h2>
     </div>
   )
 }
